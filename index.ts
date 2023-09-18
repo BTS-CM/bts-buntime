9aa93532926efc6112370a30ac088ee9860d05de
CM <grctestemail@gmail.com>
2023-09-18 21:45:16 +0100

    Update index.ts
    export ws files

--- index.ts
+++ index.ts
@@ -1,35 +1,39 @@
 /* Serializer */
-import Serializer from "./src//serializer/src/serializer";
-import fp from "./src//serializer/src/FastParser";
-import types from "./src//serializer/src/types";
-import * as ops from "./src//serializer/src/operations";
-import template from "./src//serializer/src/template";
-import SerializerValidation from "./src//serializer/src/SerializerValidation";
+import Serializer from "./src/serializer/src/serializer";
+import fp from "./src/serializer/src/FastParser";
+import types from "./src/serializer/src/types";
+import * as ops from "./src/serializer/src/operations";
+import template from "./src/serializer/src/template";
+import SerializerValidation from "./src/serializer/src/SerializerValidation";
 
 export { Serializer, fp, types, ops, template, SerializerValidation };
 
+/* WSS */
+import { Apis, ChainConfig } from "./src/ws";
+export { Apis, ChainConfig };
+
 /* ECC */
-import Address from "./src//ecc/src/address";
-import Aes from "./src//ecc/src/aes";
-import PrivateKey from "./src//ecc/src/PrivateKey";
-import PublicKey from "./src//ecc/src/PublicKey";
-import Signature from "./src//ecc/src/signature";
-import brainKey from "./src//ecc/src/BrainKey";
-import * as hash from "./src//ecc/src/hash";
-import key from "./src//ecc/src/KeyUtils";
+import Address from "./src/ecc/src/address";
+import Aes from "./src/ecc/src/aes";
+import PrivateKey from "./src/ecc/src/PrivateKey";
+import PublicKey from "./src/ecc/src/PublicKey";
+import Signature from "./src/ecc/src/signature";
+import brainKey from "./src/ecc/src/BrainKey";
+import * as hash from "./src/ecc/src/hash";
+import key from "./src/ecc/src/KeyUtils";
 
 export { Address, Aes, PrivateKey, PublicKey, Signature, brainKey, hash, key };
 
 /* Chain */
-import ChainStore from "./src//chain/src/ChainStore";
-import TransactionBuilder  from "./src//chain/src/TransactionBuilder";
-import ChainTypes from "./src//chain/src/ChainTypes";
-import ObjectId from "./src//chain/src/ObjectId";
-import NumberUtils from "./src//chain/src/NumberUtils";
-import TransactionHelper from "./src//chain/src/TransactionHelper";
-import ChainValidation from "./src//chain/src/ChainValidation";
-import EmitterInstance from "./src//chain/src/EmitterInstance";
-import Login from "./src//chain/src/AccountLogin";
+import ChainStore from "./src/chain/src/ChainStore";
+import TransactionBuilder  from "./src/chain/src/TransactionBuilder";
+import ChainTypes from "./src/chain/src/ChainTypes";
+import ObjectId from "./src/chain/src/ObjectId";
+import NumberUtils from "./src/chain/src/NumberUtils";
+import TransactionHelper from "./src/chain/src/TransactionHelper";
+import ChainValidation from "./src/chain/src/ChainValidation";
+import EmitterInstance from "./src/chain/src/EmitterInstance";
+import Login from "./src/chain/src/AccountLogin";
 
 const {FetchChainObjects, FetchChain} = ChainStore;
 
